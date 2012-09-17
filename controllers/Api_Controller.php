7261eaa8ca9b933babced15be8abd614b4f28cd3
Easton Elliott <easton@geekness.eu>
2012-09-17 01:14:49 -0600

    Add public JSON API

--- controllers/Api_Controller.php
+++ controllers/Api_Controller.php
@@ -7,16 +7,77 @@ class Api_Controller extends Base_Controller {
         parent::getTwig();
     }
 
-
     public function get_index() {
-        $this->title = 'API';
+        try {
+            $this->title = 'API';
 
-        echo $this->twig->render('api.twig', array(
-            'title' => $this->title, 
-            'error' => $_SESSION['error']));
-        $_SESSION['error'] = null;
+            return $this->twig->render('api.twig', array(
+                        'title' => $this->title,
+                        'error' => $_SESSION['error']));
+            $_SESSION['error'] = null;
+        } catch (Exception $e) {
+            die('ERROR: ' . $e->getMessage());
+        }
+    }
+
+    public function post_create() {
+        try {
+
+            $data = json_decode(file_get_contents("php://input"), true);
+
+            //Set default values
+            switch (false) {
+                case $data['name']:
+
+                    $data['name'] = 'pasteros paste';
+
+                    break;
+                case $data['content']:
+
+                    $response = array(
+                        'error' => 'No content sent'
+                    );
+
+                    break;
+            }
+
+            if (!isset($data['visible'])) {
+                $data['visible'] = false;
+            }
+
+            if (!isset($data['language'])) {
+                $data['language'] = 'plain';
+            }
+
+
+
+            //Check if there's content
+            if (false !== $data['content']) {
+
+                //Insert paste into the database and get the row ID
+                $insert_id = Model_Paste::insertPaste(array('name' => $data['name'],
+                            'content' => $data['content'],
+                            'visible' => $data['visible'],
+                            'parent_paste' => null,
+                            'language' => $data['language']), 'content');
+
+                //Create a response
+                if ($insert_id) {
+                    $response = array('id' => $insert_id);
+                } else {
+                    $response = array(
+                        'error' => 'No ID returned, something went wrong'
+                    );
+                }
+            }
+
+
+            return json_encode($response);
+            header('Content-type: application/json');
+        } catch (Exception $e) {
+            return json_last_error();
+        }
     }
 
 }
 
-?>
