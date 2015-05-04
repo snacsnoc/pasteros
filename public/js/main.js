ca171b09b28e4e6b2c918c5ff5180ca9a91fb0dd
Easton Elliott <easton@geekness.eu>
2015-05-03 19:55:21 -0600

    Add toggle_linenum function for diff page

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,12 @@
 $('#toggle').click(function() {
     $('#qrcode').toggle();
 });
+
+function toggle_linenum(){
+if(document.getElementsByTagName("ol")[0].style.listStyle.substr(0,4)=="none"){
+	document.getElementsByTagName("ol")[0].style.listStyle="decimal";
+	document.getElementsByTagName("ol")[0].style.paddingLeft="48px"
+}else{
+	document.getElementsByTagName("ol")[0].style.listStyle="none";
+	document.getElementsByTagName("ol")[0].style.paddingLeft="5px"
+}}
