0c1631d5326b9d6a283da7abffb2f27209529bd3
Easton Elliott <easton@geekness.eu>
2015-07-19 18:51:47 -0600

    Add delete controller

---
+++ controllers/Delete_Controller.php
@@ -0,0 +1,43 @@
+<?php
+
+class Delete_Controller extends Base_Controller {
+
+    function __construct() {
+        //Include Twig
+        parent::getTwig();
+    }
+
+public function get_index($paste_id, $delete_id) {
+
+
+            $get_paste = R::dispense('paste');
+            $paste_content = $get_paste->getPaste($paste_id);
+
+            //If no result is returned, return false 
+            if (false === $paste_content) {
+                return false;
+            }
+
+            $uuid = $paste_content['uuid'];
+            $delete_uuid = $paste_content['del_uuid'];
+
+            if($delete_uuid === $delete_id){
+
+
+            	$get_paste = R::dispense('paste');
+        		$paste_content = $get_paste->deletePaste($paste_id, $delete_id);
+        		if(true == $paste_content){
+        			return true;
+        		}else{
+        			return false;
+        		}	
+            }else{
+            	return false;
+            }
+
+
+
+}
+
+
+}
