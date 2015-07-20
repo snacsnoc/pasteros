50145ad1ff9f4ee2f2d0f5fad034038013d268e9
Easton Elliott <easton@geekness.eu>
2015-07-19 18:51:10 -0600

    Add stats controller and view. Pasteros.io/stats

---
+++ controllers/Stats_Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+class Stats_Controller extends Base_Controller {
+
+    function __construct() {
+        //Include Twig
+        parent::getTwig();
+    }
+
+    public function get_index() {
+        try {
+
+            $count_paste = R::dispense('paste');
+            $paste_count = $count_paste->getCountByLanguage();
+
+            $this->title = "Stats";
+           
+            //Pass the values to Twig and render the template
+            return $this->twig->render('stats.twig', array('title' => $this->title,
+                        'paste' => $paste_count
+                        ));
+
+            //Once the error has been passed to Twig, change it to null so it only appears once
+            $_SESSION['error'] = null;
+        } catch (Exception $e) {
+            $_SESSION['error'] = $e->getMessage();
+        }
+    }
+
+}
