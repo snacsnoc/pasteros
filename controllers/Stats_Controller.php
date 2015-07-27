0738a47776753e282c4e5f270ff6dbf5061a3b20
Easton Elliott <easton@geekness.eu>
2015-07-26 19:34:54 -0600

    Add get total number of unique tags

--- controllers/Stats_Controller.php
+++ controllers/Stats_Controller.php
@@ -12,12 +12,13 @@ class Stats_Controller extends Base_Controller {
 
             $count_paste = R::dispense('paste');
             $paste_count = $count_paste->getCountByLanguage();
-
+            $tag_count = $count_paste->getCountByTag();
+            
             $this->title = "Stats";
            
             //Pass the values to Twig and render the template
             return $this->twig->render('stats.twig', array('title' => $this->title,
-                        'paste' => $paste_count
+                        'paste' => $paste_count, 'tag_count' => $tag_count
                         ));
 
             //Once the error has been passed to Twig, change it to null so it only appears once
